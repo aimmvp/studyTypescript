e4f09bd8fa1df51ddafaaf1ac73630d1967aa843
s0wnd <aimmvp@gmail.com>
2018-05-24 14:57:35 +0900

    Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-interface Human {
-    name:string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age:number, gender:string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
+
+const lynn = new Human("Lynn", 18, "female");
+
 const person= {
     name: "s0wnd",
     age: 36,
@@ -13,7 +21,8 @@ const sayHi = (person: Human): string => {
 };
 
 // console.log(sayHi("s0wnd", 26, "male"));
-console.log(sayHi(person));
+// console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 
 export {};
